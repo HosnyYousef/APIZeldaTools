e854e143eaa78a6f2e3ba38ec479e93f5a1aa5a1
Yousef Hosny <Yousefhosny.hm@gmail.com>
2023-11-02 19:23:53 +0200

    trying to get the api to work

--- js/main.js
+++ js/main.js
@@ -1,25 +1,32 @@
+
+
 document.querySelector('.click').addEventListener('click', zeldaGame)
 
 function zeldaGame () {
-    let game = document.querySelector('input').value.replaceAll(' ', '_')
+    let game = document.querySelector('input').value
     
+    
+    let zeldaArray = []
+
     fetch(`https://zelda.fanapis.com/api/games?${game}`)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-      let resultBox = document.querySelector('.result')
-      resultBox.innerHTML = data.data.description
+      for(let x = 0; x < data.length; x++) {
+        zeldaArray.push(data[x])
+      }
+
+      // let resultBox = document.querySelector('.result')
+      // resultBox.innerHTML = data.description
       console.log(data)
       //console.log(data.data.creatures.food)
-      document.querySelector('h2').innerText = data.data.name
-      document.querySelector('img').src = data.data.released_date
-      document.querySelector('h3').innerText = data.data.description
+      document.querySelector('h2').innerText = zeldaArray[0].name
+      document.querySelector('h3').innerText = zeldaArray[0].released_date
+      document.querySelector('b').innerText = zeldaArray[0].description
     })
     .catch(err => {
         console.log(`error ${err}`)
     })
  
-
-
 }
 
 
