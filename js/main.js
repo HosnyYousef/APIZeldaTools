e3125bd2b8ee93c56dcc2e3732afbb8e878894e0
Yousef Hosny <Yousefhosny.hm@gmail.com>
2023-11-02 09:00:15 +0200

    changed the API and the name of a few things (to reflect the type and theme of API)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-document.querySelector('.click').addEventListener('click', getDrink)
+document.querySelector('.click').addEventListener('click', zeldaGame)
 
-function getDrink () {
-    let drink = document.querySelector('input').value.replaceAll(' ', '_')
+function zeldaGame () {
+    let game = document.querySelector('input').value.replaceAll(' ', '_')
     
-    fetch(`https://botw-compendium.herokuapp.com/api/v2/entry/${drink}`)
+    fetch(`https://zelda.fanapis.com/api/games?${game}`)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
       let resultBox = document.querySelector('.result')
@@ -11,7 +11,7 @@ function getDrink () {
       console.log(data)
       //console.log(data.data.creatures.food)
       document.querySelector('h2').innerText = data.data.name
-      document.querySelector('img').src = data.data.image
+      document.querySelector('img').src = data.data.released_date
       document.querySelector('h3').innerText = data.data.description
     })
     .catch(err => {
@@ -21,3 +21,28 @@ function getDrink () {
 
 
 }
+
+
+// document.querySelector('.click').addEventListener('click', getDrink)
+
+// function getDrink () {
+//     let drink = document.querySelector('input').value.replaceAll(' ', '_')
+    
+//     fetch(`https://botw-compendium.herokuapp.com/api/v2/entry/${drink}`)
+//     .then(res => res.json()) // parse response as JSON
+//     .then(data => {
+//       let resultBox = document.querySelector('.result')
+//       resultBox.innerHTML = data.data.description
+//       console.log(data)
+//       //console.log(data.data.creatures.food)
+//       document.querySelector('h2').innerText = data.data.name
+//       document.querySelector('img').src = data.data.image
+//       document.querySelector('h3').innerText = data.data.description
+//     })
+//     .catch(err => {
+//         console.log(`error ${err}`)
+//     })
+ 
+
+
+// }
